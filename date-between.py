babd11b033d2a37fb16f83bf0cbe6c610deee696
Holkf <69319985+Holkf@users.noreply.github.com>
2020-08-06 22:41:01 +0200

    Add files via upload

--- date-between.py
+++ date-between.py
@@ -1,5 +1,8 @@
+#Einfache Berechnung ohne Eingabe des Users
 from datetime import date
 d0 = date(2017, 8, 18)
 d1 = date(2017, 10, 26)
 delta = d1 - d0
 print(delta.days)
+
+#Da müssen Eingabemöglichkeiten her, sonst machts kein Spass
