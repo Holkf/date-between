c935c6935b9e0457d3fcee554888677700a3d439
Holger Happy <holkf@yahoo.de>
2026-04-16 11:28:44 +0200

    Daten geändert

--- date-between.py
+++ date-between.py
@@ -1,8 +1,9 @@
 #Einfache Berechnung ohne Eingabe des Users
 from datetime import date
-d0 = date(2017, 8, 18)
-d1 = date(2017, 10, 26)
+d0 = date(2025, 4, 11)
+d1 = date(2026, 4, 16)
 delta = d1 - d0
 print(delta.days)
 
 #Da müssen Eingabemöglichkeiten her, sonst machts kein Spass
+#So ist es ja nur statisch
